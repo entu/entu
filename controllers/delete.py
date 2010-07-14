cf917e6d45ff9782b1819929dfffb2c4fd240c1a
argoroots <argo@roots.ee>
2010-07-14 11:15:10 +0000

    Delete controller
    git-svn-id: http://argo.unfuddle.com/svn/argo_ois/trunc/bonga-bonga@50 dada7bbb-e600-4cd3-a4f0-d41eb9236a84

--- controllers/delete.py
+++ controllers/delete.py
@@ -1,35 +1,18 @@
-import os
-from google.appengine.ext.webapp import template
-
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-
+from boFunctions import *
 from models import *
 
-class deletePerson(webapp.RequestHandler):
-	def get(self):
-		q = Person.all()
-		db.delete(Person.all().fetch(300))
-		path = os.path.join(os.path.dirname(__file__), '../views/delete.html')
-		template_values = { 'count': q.count() }
-		self.response.out.write(template.render(path, template_values))
 
-
-class deleteContact(webapp.RequestHandler):
-	def get(self):
-		q = Contact.all()
+class Delete(webapp.RequestHandler):
+	def get(self ,url):
+		q = db.GqlQuery('SELECT * FROM ' + url)
 		db.delete(q.fetch(300))
-		path = os.path.join(os.path.dirname(__file__), '../views/delete.html')
-		template_values = { 'count': q.count() }
-		self.response.out.write(template.render(path, template_values))
+		boView(self, '', 'delete.html', { 'count': q.count() })
 
 
-application = webapp.WSGIApplication([
-									  (r'/delete/person', deletePerson),
-									  (r'/delete/contact', deleteContact)
-									 ], debug=True)
 def main():
-    run_wsgi_app(application)
+	boWSGIApp([
+			(r'/delete/(.*)', Delete),
+		])
 
 
 if __name__ == '__main__':
