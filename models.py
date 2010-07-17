dbcc261a07d5b9a20284da15b313f6dfcd6a21fc
mitselek <mihkel.putrinsh@gmail.com>
2010-07-17 19:55:56 +0000

    remodeled Dictionary, Translations, Classifier and AccessLog
    git-svn-id: http://argo.unfuddle.com/svn/argo_ois/trunc/bonga-bonga@61 dada7bbb-e600-4cd3-a4f0-d41eb9236a84

--- models.py
+++ models.py
@@ -5,58 +5,79 @@ from google.appengine.api import users
 
 # Common
 
-class Dictionary(search.SearchableModel):
-    kind_name   = db.StringProperty()
-    entity_key  = db.StringProperty()
+class AccessLog(db.Model):
+    date            = db.DateTimeProperty(auto_now_add=True)
+    person          = db.StringProperty()
+    property_name   = db.StringProperty()
+    old_value       = db.StringProperty()
+    new_value       = db.StringProperty()
+
+class Dictionary(db.Model):
     name        = db.StringProperty()
+#------
+    access_log  = db.StringListProperty()
+
+class Translation(search.SearchableModel):
     language    = db.StringProperty()
     value       = db.StringProperty()
+    dictionary  = db.ReferenceProperty(Dictionary, collection_name='translations')
+#------
+    access_log  = db.StringListProperty()
 
 class Classifier(db.Model):
-    create_date = db.DateTimeProperty(auto_now_add=True)
     name        = db.StringProperty()
     value       = db.ReferenceProperty(Dictionary, collection_name='classifiers')
+#------
+    access_log  = db.StringListProperty()
 
 
 # Person
 
 class Person(search.SearchableModel):
-    create_date = db.DateTimeProperty(auto_now_add=True)
     forename    = db.StringProperty()
     surname     = db.StringProperty()
     idcode      = db.StringProperty()
     gender      = db.StringProperty()
     birth_date  = db.DateProperty()
     identities  = db.StringListProperty()
+#------
+    access_log  = db.StringListProperty()
 
 class PersonPreferences(db.Model):
     person      = db.ReferenceProperty(Person, collection_name='preferences')
-    language    = db.StringProperty()
+    language    = db.ReferenceProperty(Classifier, collection_name='person_languages')
     avatar      = db.BlobProperty()
+#------
+    access_log  = db.StringListProperty()
 
 class Contact(db.Model):
-    create_date     = db.DateTimeProperty(auto_now_add=True)
     person          = db.ReferenceProperty(Person, collection_name='contacts')
     type            = db.StringProperty()
     value           = db.StringProperty()
     activation_key  = db.StringProperty()
+#------
+    access_log      = db.StringListProperty()
 
 class Role(db.Model):
     name                    = db.StringProperty()
     controllers_to_read     = db.StringListProperty()
     controllers_to_write    = db.StringListProperty()
+#------
+    access_log              = db.StringListProperty()
 
 class PersonRole(db.Model):
     start_date  = db.DateTimeProperty(auto_now_add=True)
     end_date    = db.DateTimeProperty(auto_now_add=True)
     person      = db.ReferenceProperty(Person, collection_name='roles')
     role        = db.ReferenceProperty(Role, collection_name='persons')
+#------
+    access_log  = db.StringListProperty()
 
 
 # Curriculum
 
 class Curriculum(search.SearchableModel):
-    name                    = db.StringProperty()
+    name                    = db.ReferenceProperty(Dictionary, collection_name='curriculum_names')
     code                    = db.StringProperty()
     tags                    = db.StringListProperty()
     level_of_education      = db.ReferenceProperty(Classifier, collection_name='curriculum_level_of_educations')
@@ -65,10 +86,14 @@ class Curriculum(search.SearchableModel):
     nominal_credit_points   = db.IntegerProperty()
     degree                  = db.ReferenceProperty(Classifier, collection_name='curriculum_degree')
     manager                 = db.ReferenceProperty(Person, collection_name='managed_curriculums')
+#------
+    access_log  = db.StringListProperty()
 
 
 class RatingScale(db.Model):
-    name            = db.StringProperty()
+    name        = db.ReferenceProperty(Dictionary, collection_name='curriculum_names')
+#------
+    access_log  = db.StringListProperty()
 
 
 class Subject(search.SearchableModel):
@@ -78,3 +103,5 @@ class Subject(search.SearchableModel):
     tags            = db.StringListProperty()
     credit_points   = db.FloatProperty()
     rating_scale    = db.ReferenceProperty(RatingScale, collection_name='subjects')
+#------
+    access_log  = db.StringListProperty()
