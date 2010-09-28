f06a61386e6d562b8e17474a2a0b40292ef2edf1
Argo Roots <argo@roots.ee>
2010-09-28 10:23:49 +0000

    Removed BO framework folder; Removed database.general;

--- bo/settings.py
+++
@@ -1,9 +0,0 @@
-SYSTEM_TITLE =      'Bonga'
-SYSTEM_LANGUAGE =   'estonian'
-SYSTEM_EMAIL =      'argoroots@gmail.com'
-SYSTEM_URL =        'http://bonga.artun.ee'
-
-TEMPLATE_LOGIN =    'login.html'
-
-GUEST_NAME =        'Kylaline'
-GUEST_EMAIL =       'ois@artun.ee'
--- database/general.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import db
-
-from database.dictionary import *
-from database.person import *
-
-
