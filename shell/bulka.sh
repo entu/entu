cc9a4174f040e952bfffc37f184b2362931119db
Argo Roots <argo@roots.ee>
2010-09-28 10:11:51 +0000

    One script to load all data

--- shell/bulka.sh
+++ shell/bulka.sh
@@ -1,1 +1,13 @@
-python2.5 /usr/local/bin/bulkloader.py --url=https://bonga-bonga.appspot.com/bulka --kind=${1} --filename="../../SVN2/trunc/Export/CSV/${2}" --config_file=../database/loaders.py --has_header
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "           ${1}"
+echo "--------------------------------------------------------------------------------"
+
+iconv --from-code UTF-16 --to-code UTF-8 ${2} > ${2}.utf8.csv
+
+python2.5 /usr/local/bin/bulkloader.py --url=https://bongapp.appspot.com/bulka --kind=${1} --filename="${2}.utf8.csv" --config_file=../loaders.py
---
+++ shell/load_all_data.sh
@@ -0,0 +1,10 @@
+./bulka.sh Contact       ~/Dropbox/Work/ois_export/contact.csv
+./bulka.sh Curriculum    ~/Dropbox/Work/ois_export/curriculum.csv
+./bulka.sh Department    ~/Dropbox/Work/ois_export/department.csv
+./bulka.sh Dictionary    ~/Dropbox/Work/ois_export/dictionary.csv
+./bulka.sh Module        ~/Dropbox/Work/ois_export/module.csv
+./bulka.sh ModuleSubject ~/Dropbox/Work/ois_export/moduleSubject.csv
+./bulka.sh Orientation   ~/Dropbox/Work/ois_export/orientation.csv
+./bulka.sh Person        ~/Dropbox/Work/ois_export/person.csv
+./bulka.sh Subject       ~/Dropbox/Work/ois_export/subject.csv
+./bulka.sh Translation   ~/Dropbox/Work/ois_export/translation.csv
