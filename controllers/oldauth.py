47e89f7915914c75d25eb752b65e117c75abd30e
Argo Roots <argo@roots.ee>
2010-10-06 07:32:03 +0000

    ois.artun.ee authentication hack made universal for other old sites and added security (no salt in svn)

---
+++ controllers/oldauth.py
@@ -0,0 +1,44 @@
+import hashlib
+import base64
+from datetime import date
+
+from bo import *
+from database.oldauth import *
+
+
+class Login(webapp.RequestHandler):
+    def get(self):
+        if self.request.get('site'):
+            user = users.get_current_user()
+            site = self.request.get('site')
+            oa = db.Query(OldAuth).filter('site', site).get()
+            if not oa:
+                oa = OldAuth()
+                oa.site = site
+                oa.put()
+            user_name = user.nickname()
+            user_key = hashlib.md5(user.nickname() + date.today().strftime('%Y-%m-%d') + oa.salt).hexdigest()
+            key = base64.b64encode(user_key + user_name)
+            if oa.loginurl:
+                self.redirect(oa.loginurl % key)
+
+
+class Logout(webapp.RequestHandler):
+    def get(self):
+        if self.request.get('site'):
+            user = users.get_current_user()
+            site = self.request.get('site')
+            oa = db.Query(OldAuth).filter('site', site).get()
+            if oa:
+                self.redirect(users.create_logout_url(oa.logouturl))
+
+
+def main():
+    Route([
+             ('/oldauth/exit', Logout),
+             ('/oldauth', Login),
+            ])
+
+
+if __name__ == '__main__':
+    main()
---
+++ database/oldauth.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+
+
+class OldAuth(db.Model):
+    site      = db.StringProperty()
+    salt      = db.StringProperty(default='')
+    loginurl  = db.StringProperty()
+    logouturl = db.StringProperty()
