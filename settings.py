13ad6c8ed8b04e07bbdbc4d421a0c36bbc8cacbd
Argo Roots <argo@roots.ee>
2011-02-07 07:34:53 +0000

    UserPreferences.timezone system default setting

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-SYSTEM_TITLE =      'Bonga'
-SYSTEM_URL =        'bonga.artun.ee'
-SYSTEM_EMAIL =      'argo.roots@artun.ee'
-SYSTEM_EMAIL_PREFIX =   'BONGA: '
-SYSTEM_LOGO_URL =   'http://ois.artun.ee/logo_est_horisont.png'
-SYSTEM_LANGUAGE =   'estonian'
+SYSTEM_TITLE =          'EKA'
+SYSTEM_URL =            'bonga.artun.ee'
+SYSTEM_EMAIL =          '"EKA ÕppeInfoSüsteem" <bonga@artun.ee>'
+SYSTEM_EMAIL_PREFIX =   ''
+SYSTEM_EMAIL_FOOTER =   ''
+SYSTEM_LOGO_URL =       'http://ois.artun.ee/logo_est_horisont.png'
+SYSTEM_LANGUAGE =       'estonian'
+SYSTEM_TIMEZONE =       120
