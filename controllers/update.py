1aec2706d80e658ee4a5c46e502062ba9db0b1de
Argo Roots <argo@roots.ee>
2011-06-15 15:20:15 +0000

    Random ad-hoc update scripts

--- controllers/update.py
+++ controllers/update.py
@@ -1,22 +1,95 @@
-from helpers import *
-from database import *
+from google.appengine.api import users
+from datetime import date
+
+from bo import *
+from database.bubble import *
+from database.zimport.zbubble import *
+from database.zimport.zoin import *
 
 
-class Update(webapp.RequestHandler):
-
+class Update(boRequestHandler):
     def get(self):
-        for c in db.Query(Translation).fetch(1000):
-            try:
-                c.dictionary_name = c.dictionary.name
-                c.save()
-            except:
-                self.response.out.write(str(c.key()) + '\n')
+
+        self.header('Content-Type', 'text/plain; charset=utf-8')
+
+        a = db.Query(Aggregation)
+        ac = a.count(limit=2000000)
+
+        b = db.Query(AggregationValue)
+        bc = b.count(limit=2000000)
+
+        self.echo(str(ac))
+        self.echo(str(bc))
+
+
+class Sort_est(boRequestHandler):
+    def get(self):
+        for b in db.Query(Bubble).filter('model_version', 'B').fetch(1000):
+            if b.name:
+                if b.name.estonian:
+                    b.sort_estonian = StringToSortable(b.name.estonian)
+                if b.name.english:
+                    b.sort_english = StringToSortable(b.name.english)
+                b.model_version = 'A'
+                b.put()
+
+
+class Sort_est2(boRequestHandler):
+    def get(self):
+        self.header('Content-Type', 'text/plain; charset=utf-8')
+        self.echo('A:' + str(db.Query(Bubble).filter('model_version', 'A').count(limit=100000)))
+        self.echo('B:' + str(db.Query(Bubble).filter('model_version', 'B').count(limit=100000)))
+
+
+class PersonUser(boRequestHandler):
+    def get(self):
+        for p in db.Query(Person).filter('model_version', 'A').fetch(1000):
+            if p.apps_username and not p.user:
+                p.user = users.User(p.apps_username)
+            p.model_version = 'B'
+            p.put()
+
+
+class PersonUser2(boRequestHandler):
+    def get(self):
+        self.header('Content-Type', 'text/plain; charset=utf-8')
+        self.echo('A:' + str(db.Query(Person).filter('model_version', 'A').count(limit=100000)))
+        self.echo('B:' + str(db.Query(Person).filter('model_version', 'B').count(limit=100000)))
+
+
+class GradeI(boRequestHandler):
+    def get(self):
+        self.header('Content-Type', 'text/plain; charset=utf-8')
+
+        p = []
+        for g in db.Query(Grade).filter('datetime >=', date(2011, 1, 3)).filter('bubble_type', 'course').filter('is_deleted', False).fetch(1000):
+            p.append(g.person.key())
+
+        p = GetUniqueList(p)
+        self.echo(str(len(p)))
+
+
+class Log(boRequestHandler):
+    def get(self):
+
+        self.header('Content-Type', 'text/plain; charset=utf-8')
+
+        bubble = Bubble().get_by_id(1622190)
+        bubble.leechers.remove(db.Key('agdib25nYXBwcg4LEgZQZXJzb24YvqZuDA'))
+        bubble.put()
+
 
 
 
 def main():
     Route([
-            (r'/update', Update),
+            ('/update', Update),
+            ('/update/sort', Sort_est),
+            ('/update/sort2', Sort_est2),
+            ('/update/user', PersonUser),
+            ('/update/user2', PersonUser2),
+            ('/update/grade', GradeI),
+            ('/update/log3', Log),
         ])
 
 
