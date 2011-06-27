660568b429fa8fe1323bac33052456467ceaaed4
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2011-06-27 16:51:07 +0300

    bubbledu/person/id shows basic person data

--- appengine/controllers/person.py
+++ appengine/controllers/person.py
@@ -2,6 +2,44 @@ from bo import *
 from database.person import *
 
 
+class ShowPerson(boRequestHandler):
+    def get(self, id):
+        if self.authorize('bubbler'):
+
+            person = Person().get_by_id(int(id))
+
+            changeinfo = ''
+            last_change = person.last_change
+            if last_change:
+                if last_change.user:
+                    changer = db.Query(Person).filter('apps_username', last_change.user).get()
+                    if changer:
+                        changeinfo = Translate('person_changed_on') % {'name': changer.displayname, 'date': UtcToLocalDateTime(last_change.datetime).strftime('%d.%m.%Y %H:%M')}
+
+            self.view(person.displayname, 'person/person.html', {
+                'person': person,
+                'changed': changeinfo,
+            })
+
+    def post(self, key):
+        if self.authorize('bubbler'):
+            person = Person().get(key)
+            field = self.request.get('field').strip()
+            value = self.request.get('value').strip()
+            if bubble:
+                if value:
+                    if field in ['forename', 'surname']:
+                        setattr(person, field, value)
+                    if field in ['person_birthdate']:
+                        setattr(person, field, datetime.strptime(value, '%d.%m.%Y'))
+                    if field == 'primary_email':
+                        person.email = value
+                else:
+                    setattr(person, field, None)
+
+                person.put()
+
+
 class SetRole(boRequestHandler):
     def post(self):
 
@@ -15,6 +53,7 @@ class SetRole(boRequestHandler):
 def main():
     Route([
             ('/person/set_role', SetRole),
+            (r'/person/(.*)', ShowPerson),
         ])
 
 
