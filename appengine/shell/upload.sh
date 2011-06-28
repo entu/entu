baaa66caee8ca30c378e01f02d502c090ddbe42c
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2011-06-28 17:31:07 +0300

    upload.sh paths fixed

--- appengine/shell/upload.sh
+++ appengine/shell/upload.sh
@@ -1,14 +1,19 @@
+#!/bin/sh
+
 url=https://bongapp.appspot.com/bulka
 email=bonga.bulka@artun.ee
 
-dir_log=~/Dropbox/Work/EKA/OIS/SVN/trunk/shell/log
-file_config=~/Dropbox/Work/EKA/OIS/SVN/trunk/shell/upload.yaml
-file_password=~/Dropbox/Work/EKA/OIS/SVN/trunk/shell/secure.txt
+script_path=`echo "$(dirname $0)"`
+data_path=`pwd`
+
+dir_log=${script_path}/log
+file_config=${script_path}/upload.yaml
+file_password=${script_path}/secure.txt
 file_log=${dir_log}/log-${1}.txt
-file_csv=~/Dropbox/Work/EKA/zExport/${1}.csv
+file_csv=${data_path}/${1}.csv
 
 kind=${1}
 
-echo ${1}
+echo ${file_csv}
 cd ${dir_log}
 cat ${file_password} | appcfg.py upload_data --config_file=${file_config} --filename=${file_csv} --kind=$kind --url=${url} --email=${email} --passin > ${file_log} 2>&1
