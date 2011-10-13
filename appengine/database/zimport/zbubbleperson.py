376a5b9103e90e6e912bb6e7c28a9d652f767f3f
Argo Roots <argo@roots.ee>
2011-10-13 15:32:48 +0300

    BubblePerson import

---
+++ appengine/database/zimport/zbubbleperson.py
@@ -0,0 +1,26 @@
+from google.appengine.ext import db
+
+from bo import *
+from database.zimport.zoin import *
+from database.bubble import *
+from database.person import *
+
+
+class zBubblePerson(db.Model):
+    bubble_old_id = db.StringProperty()
+    person  = db.StringProperty()
+
+    def zimport(self):
+        p = Person().get(self.person)
+        bubble_key = GetZoinKey('Bubble', self.bubble_old_id)
+
+        if p and bubble_key:
+            p.leecher = AddToList(bubble_key, p.leecher)
+            p.put('zimport')
+
+            b = Bubble().get(bubble_key)
+            if b:
+                b.leechers = AddToList(db.Key(self.person), b.leechers)
+                b.put('zimport')
+
+            self.delete()
