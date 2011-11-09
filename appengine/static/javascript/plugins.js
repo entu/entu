f9c68914cd6d263dbf54cbd842905e61f69572ae
Argo Roots <argo@roots.ee>
2011-11-09 11:59:29 +0200

    Merged JS plugins to one plugins.js (plugins.min.js) file; Changed icons font;

---
+++ appengine/static/javascript/plugins.js
@@ -0,0 +1,5 @@
+//@codekit-prepend "jquery.elastic.source.js";
+//@codekit-prepend "jquery.placeholder.js";
+//@codekit-prepend "jquery.raty.js";
+//@codekit-prepend "jquery.simplemodal-1.4.1.js";
+//@codekit-prepend "jstorage.js";
