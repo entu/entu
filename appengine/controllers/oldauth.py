d5bab5bb325250f4eda150582042e013bb194627
Argo Roots <argo@roots.ee>
2011-11-21 14:10:28 +0200

    Merge master to develop

--- appengine/controllers/oldauth.py
+++ appengine/controllers/oldauth.py
@@ -2,9 +2,9 @@ import hashlib
 import base64
 from random import randint
 from datetime import *
+from google.appengine.api import users
 
 from bo import *
-from database.person import *
 from database.oldauth import *
 
 
@@ -12,15 +12,15 @@ class Login(boRequestHandler):
     def get(self):
         if self.authorize():
             if self.request.get('site'):
-                p = Person().current
+                user = users.get_current_user()
                 site = self.request.get('site')
                 oa = db.Query(OldAuth).filter('site', site).get()
                 if not oa:
                     oa = OldAuth()
                     oa.site = site
                     oa.put()
-                user_key = hashlib.md5(p.user + (datetime.today() + timedelta(hours=2)).strftime('%Y-%m-%d') + oa.salt).hexdigest()
-                key = base64.b64encode(user_key + p.user)
+                user_key = hashlib.md5(user.email() + (datetime.today() + timedelta(hours=2)).strftime('%Y-%m-%d') + oa.salt).hexdigest()
+                key = base64.b64encode(user_key + user.email())
 
                 if oa.loginurl:
                     self.redirect(oa.loginurl % key)
