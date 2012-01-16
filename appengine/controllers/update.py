dd12190fe3970415e24ac5c1f134d7c7259c05ea
Argo Roots <argo@roots.ee>
2012-01-16 10:19:09 +0200

    Show datetime in local timezone;

--- appengine/controllers/update.py
+++ appengine/controllers/update.py
@@ -29,16 +29,35 @@ class Dokumendid(boRequestHandler):
             attachments = [('Docs.csv', '\n'.join(csv))],
         )
 
+
 class FixStuff(boRequestHandler):
     def get(self):
-        b = Bubble().get('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYjcmwAgw')
-        b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYy4S1Agw'), b.optional_bubbles)
-        b.put()
+        # taskqueue.Task(url='/update/stuff').add()
 
-        b = Bubble().get('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYrNuyAgw')
-        b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYnP20Agw'), b.optional_bubbles)
-        b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYwKe0Agw'), b.optional_bubbles)
-        b.put()
+        for b in db.Query(Bubble).filter('type', 'doc_kirjavahetus').fetch(1000):
+            setattr(b, 'reply_counter', db.Key('agpzfmJ1YmJsZWR1chALEgdDb3VudGVyGK30twIM'))
+            b.put()
+
+    def post(self):
+        # b = Bubble().get('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYjcmwAgw')
+        # b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYy4S1Agw'), b.optional_bubbles)
+        # b.put()
+
+        # b = Bubble().get('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYrNuyAgw')
+        # b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYnP20Agw'), b.optional_bubbles)
+        # b.optional_bubbles = AddToList(db.Key('agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYwKe0Agw'), b.optional_bubbles)
+        # b.put()
+
+        for b in db.Query(Bubble).filter('allowed_subtypes', 'doc_kirjavahetus').fetch(1000):
+            b.reply_counter = db.Key('agpzfmJ1YmJsZWR1chALEgdDb3VudGVyGK30twIM')
+            b.put()
+            # bt = b.GetType()
+            # for pp_key in getattr(bt, 'propagated_properties', []):
+            #     pp = BubbleProperty().get(pp_key)
+            #     for sb in Bubble().get(b.optional_bubbles):
+            #         if not hasattr(sb, pp.data_property):
+            #             setattr(sb, pp.data_property, getattr(b, pp.data_property))
+            #         sb.put()
 
 
 class AddUser(boRequestHandler):
