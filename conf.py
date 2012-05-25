5691bb1a40b409c3e0fb315d50b4488ba303d658
Argo Roots <argo@roots.ee>
2012-05-25 14:15:26 +0300

    Read The Docs again

--- conf.py
+++ conf.py
@@ -4,4 +4,25 @@ if on_rtd:
     html_theme = 'default'
 else:
     html_theme = 'nature'
-project == "Python"
+
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['tornado']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
