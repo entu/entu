6a8d202d918462b0ce82f4cad0f1f72694de604e
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2012-06-02 17:15:27 +0300

    Use md5 for key generation

--- tornado/import/amphora2bubbledu.sh
+++ tornado/import/amphora2bubbledu.sh
@@ -33,7 +33,9 @@ PD_FOLDER_IS_PUBLIC='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUY2qTUAgw_agpzfmJ1YmJsZWR1cg8
 PD_FOLDER_NAME='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUY2qTUAgw_agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYmO7TAgw'
 BD_DOCUMENT='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYxNXRAgw'
 PD_FILE='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYxNXRAgw_agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYsbTUAgw'
-ROOT_B_KEY="amphora_ROOT"
+# PD_DOC_CREATED='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYxNXRAgw_agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUY_5TUAgw'
+
+ROOT_B_KEY="amphora_ROOT_"
 
 ARGO_PERSON_KEY='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUYy_rLAgw'
 MIHKEL_PERSON_KEY='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUY1IfMAgw'
@@ -42,11 +44,11 @@ MIHKEL_PERSON_KEY='agpzfmJ1YmJsZWR1cg8LEgZCdWJibGUY1IfMAgw'
 echo "You might want to add rights after import with:
 ------------------------------
 DELETE FROM `relationship`
-WHERE `related_bubble_id` IN (SELECT id FROM bubble WHERE gae_key IN ('${ARGO_PERSON_KEY}','${MIHKEL_PERSON_KEY}'));
-INSERT IGNORE INTO relationship (bubble_id, related_bubble_id, `type`)
-SELECT id AS bubble_id, (SELECT id FROM bubble WHERE gae_key = '${ARGO_PERSON_KEY}') AS related_bubble_id, 'viewer' AS `type` FROM bubble;
-INSERT IGNORE INTO relationship (bubble_id, related_bubble_id, `type`)
-SELECT id AS bubble_id, (SELECT id FROM bubble WHERE gae_key = '${MIHKEL_PERSON_KEY}') AS related_bubble_id, 'viewer' AS `type` FROM bubble;
+WHERE `related_entity_id` IN (SELECT id FROM entity WHERE gae_key IN ('${ARGO_PERSON_KEY}','${MIHKEL_PERSON_KEY}'));
+INSERT IGNORE INTO relationship (entity_id, related_entity_id, `type`)
+SELECT id AS entity_id, (SELECT id FROM entity WHERE gae_key = '${ARGO_PERSON_KEY}') AS related_entity_id, 'viewer' AS `type` FROM entity;
+INSERT IGNORE INTO relationship (entity_id, related_entity_id, `type`)
+SELECT id AS entity_id, (SELECT id FROM entity WHERE gae_key = '${MIHKEL_PERSON_KEY}') AS related_entity_id, 'viewer' AS `type` FROM entity;
 ------------------------------
 "
 
@@ -61,7 +63,7 @@ else
         do
             # echo -n $fn
             if find "$fn"/* -type d -maxdepth 0 | read; then
-                echo "${ROOT_B_KEY}/$fn"
+                echo "$fn"
             fi
         done >> $FOLDERS_LIST
     echo "Listed"
@@ -75,28 +77,30 @@ if [ -f $FOLDERS_MYSQL ]; then
     echo "Already parsed"
 else
     # Create root folder
-    B_KEY="${ROOT_B_KEY}"
+    B_KEY=`echo ${ROOT_B_KEY} | md5 | cut -d" " -f1`
     echo "-- Root element" > $FOLDERS_MYSQL
-    echo "INSERT INTO bubble SET bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${B_KEY}' ON DUPLICATE KEY UPDATE bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${B_KEY}';" >> $FOLDERS_MYSQL
+    echo "INSERT INTO entity SET entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${ROOT_B_KEY}${B_KEY}' ON DUPLICATE KEY UPDATE entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${ROOT_B_KEY}${B_KEY}';" >> $FOLDERS_MYSQL
 
-    echo "DELETE FROM property WHERE bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}');" >> $FOLDERS_MYSQL
-    echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}'), language = 'estonian', value_string = 'Amphora dokumendid';" >> $FOLDERS_MYSQL
-    echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}'), language = 'english', value_string = 'Amphora Root';" >> $FOLDERS_MYSQL
+    echo "DELETE FROM property WHERE entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}');" >> $FOLDERS_MYSQL
+    echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}'), language = 'estonian', value_string = 'Amphora dokumendid';" >> $FOLDERS_MYSQL
+    echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}'), language = 'english', value_string = 'Amphora Root';" >> $FOLDERS_MYSQL
 
     # Create Amphora folders
     while read fn
     do
         echo "-- ${fn}"
-        B_KEY="${fn}"
-        PARENT_B_KEY=`echo $B_KEY | rev | cut -d'/' -f2- | rev`
-        echo "INSERT INTO bubble SET bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${B_KEY}' ON DUPLICATE KEY UPDATE bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${B_KEY}';"
+        B_KEY=`echo ${fn} | md5 | cut -d" " -f1`
+        PARENT_B_KEY=`echo $fn | rev | cut -d'/' -f2- | rev | md5 | cut -d" " -f1`
 
-        echo "DELETE FROM relationship WHERE related_bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}');"
-        echo "INSERT INTO relationship SET bubble_id = (SELECT id FROM bubble WHERE gae_key = '${PARENT_B_KEY}'), related_bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}'), type='subbubble', gae_key='${PARENT_B_KEY}_${B_KEY}';"
+        fn=`echo ${fn} | sed -e "s/'/\\\'/g"`
+        echo "INSERT INTO entity SET entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${ROOT_B_KEY}${B_KEY}' ON DUPLICATE KEY UPDATE entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_FOLDER}'), gae_key = '${ROOT_B_KEY}${B_KEY}';"
 
-        echo "DELETE FROM property WHERE bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}');"
-        echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}'), language = 'estonian', value_string = '${fn}';"
-        echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), bubble_id = (SELECT id FROM bubble WHERE gae_key = '${B_KEY}'), language = 'english', value_string = '${fn}';"
+        echo "DELETE FROM relationship WHERE related_entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}');"
+        echo "INSERT INTO relationship SET entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${PARENT_B_KEY}'), related_entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}'), type='child', gae_key='${ROOT_B_KEY}${PARENT_B_KEY}_${B_KEY}';"
+
+        echo "DELETE FROM property WHERE entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}');"
+        echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}'), language = 'estonian', value_string = '${fn}';"
+        echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FOLDER_NAME}'), entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${B_KEY}'), language = 'english', value_string = '${fn}';"
 
     done < $FOLDERS_LIST >> $FOLDERS_MYSQL
     echo "Parsed"
@@ -113,27 +117,48 @@ else
         do
             echo "-- ${fn}"
             dname="`dirname "${fn}" | rev | cut -d'/' -f2- | rev`"
-            fname=`basename "${fn}"`
 
             file_orig_name=`grep file_orig_name "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
             file_orig_guid=`grep file_orig_guid "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
-            # document_date=`grep document_date "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
-            # author=`grep author "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
+            document_date=`grep document_date "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
+            author=`grep author "${fn}" | sed -e 's/<[a-zA-Z\/][^>]*>//g' | sed 's/^ *//;s/ *$//' | tr -d '[ \r]'`
 
-            FILE_KEY="amphora_${file_orig_guid}"
-            DOCUMENT_KEY="${FILE_KEY}"
-            DIRECTORY_KEY="${ROOT_B_KEY}/${dname}"
+            FILE_KEY=`echo ${file_orig_guid} | md5 | cut -d" " -f1`
+            DIRECTORY_KEY=`echo ${dname} | md5 | cut -d" " -f1`
+
             # Create document bubble
-            echo "INSERT INTO bubble SET bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_DOCUMENT}'), gae_key = '${DOCUMENT_KEY}' ON DUPLICATE KEY UPDATE bubble_definition_id = (SELECT id FROM bubble_definition WHERE gae_key = '${BD_DOCUMENT}'), gae_key = '${DOCUMENT_KEY}';"
-            echo "DELETE FROM relationship WHERE related_bubble_id = (SELECT id FROM bubble WHERE gae_key = '${DOCUMENT_KEY}');"
-            echo "INSERT INTO relationship SET bubble_id = (SELECT id FROM bubble WHERE gae_key = '${DIRECTORY_KEY}'), related_bubble_id = (SELECT id FROM bubble WHERE gae_key = '${DOCUMENT_KEY}'), type='subbubble', gae_key='${DIRECTORY_KEY}_${DOCUMENT_KEY}';"
+            echo "INSERT INTO entity SET
+                entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_DOCUMENT}'),
+                gae_key = '${ROOT_B_KEY}${FILE_KEY}',
+                created = STR_TO_DATE('${document_date}','%d.%m.%Y %H:%i:%s'),
+                created_by = '$author'
+                ON DUPLICATE KEY UPDATE entity_definition_id = (SELECT id FROM entity_definition WHERE gae_key = '${BD_DOCUMENT}'), gae_key = '${ROOT_B_KEY}${FILE_KEY}';"
+            echo "DELETE FROM relationship WHERE related_entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}');"
+            echo "INSERT INTO relationship SET
+                entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${DIRECTORY_KEY}'),
+                related_entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}'),
+                type='child',
+                gae_key='${ROOT_B_KEY}${DIRECTORY_KEY}_${FILE_KEY}';"
 
             # Insert file metadata
-            echo "INSERT IGNORE INTO file SET gae_key='${FILE_KEY}', filename='${file_orig_name}';"
+            echo "INSERT IGNORE INTO file SET gae_key='${ROOT_B_KEY}${FILE_KEY}', filename='${file_orig_name}';"
 
+            echo "DELETE FROM property WHERE entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}');"
             # Add file property to document
-            echo "DELETE FROM property WHERE bubble_id = (SELECT id FROM bubble WHERE gae_key = '${DOCUMENT_KEY}');"
-            echo "INSERT INTO property SET property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FILE}'), bubble_id = (SELECT id FROM bubble WHERE gae_key = '${DOCUMENT_KEY}'), value_file = (SELECT id FROM file WHERE gae_key = '${FILE_KEY}');"
+            echo "INSERT INTO property SET
+                property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FILE}'),
+                entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}'),
+                value_file = (SELECT id FROM file WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}');"
+            # Add creation date property to document
+            echo "INSERT INTO property SET
+                property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FILE}'),
+                entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}'),
+                value_file = (SELECT id FROM file WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}');"
+            # Add author property to document
+            echo "INSERT INTO property SET
+                property_definition_id = (SELECT id FROM property_definition WHERE gae_key = '${PD_FILE}'),
+                entity_id = (SELECT id FROM entity WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}'),
+                value_file = (SELECT id FROM file WHERE gae_key = '${ROOT_B_KEY}${FILE_KEY}');"
 
         done >> $FILES_SQL
     echo "Parsed"
