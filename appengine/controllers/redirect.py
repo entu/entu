40e5ac225d2040f507fa50f6fc5939ec12a0a911
Argo Roots <argo@roots.ee>
2012-06-14 07:00:10 +0300

    Redirect application from GAE to Entu

--- appengine/controllers/redirect.py
+++ appengine/controllers/redirect.py
@@ -4,13 +4,21 @@ from bo import *
 class RedirectToEntu(boRequestHandler):
     def get(self, url):
         self.redirect('http://entu.artun.ee')
+
+
 class RedirectToEntuPublic(boRequestHandler):
     def get(self, url):
         self.redirect('http://entu.artun.ee/public')
 
 
+class RedirectToEntuApplication(boRequestHandler):
+    def get(self, url):
+        self.redirect('http://entu.artun.ee/application')
+
+
 def main():
     Route([
+        ('/application(.*)', RedirectToEntuApplication),
         ('/public(.*)', RedirectToEntuPublic),
         ('/(.*)', RedirectToEntu),
     ])
