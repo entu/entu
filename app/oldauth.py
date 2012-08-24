a19b6e4001f16999ed066692c006639b1af535b8
Argo Roots <argo@roots.ee>
2012-08-24 21:49:35 +0300

    Bugfix: Ester import will not create duplicate Entity if it's already imported.

--- app/oldauth.py
+++ app/oldauth.py
@@ -28,7 +28,7 @@ class Login(myRequestHandler):
         if email.split('@')[1] != self.settings['oldauth_domain']:
             return self.redirect(self.settings['oldauth_error'] % base64.b64encode(email))
 
-        user_key = hashlib.md5(email + (datetime.today() + timedelta(hours=2)).strftime('%Y-%m-%d') + self.settings['oldauth_key']).hexdigest()
+        user_key = hashlib.md5(email + datetime.today().strftime('%Y-%m-%d') + self.settings['oldauth_key']).hexdigest()
         key = base64.b64encode(user_key + email)
 
         self.redirect(self.settings['oldauth_login'] % key)
