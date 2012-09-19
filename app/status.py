47d2f17c1d589b781ac8a478bad9b364de0dbcc0
Argo Roots <argo@roots.ee>
2012-09-19 10:45:11 +0300

    Status page update

--- app/status.py
+++ app/status.py
@@ -8,7 +8,14 @@ class ShowStatus(myRequestHandler):
         self.require_setting('app_title', 'this application')
         self.require_setting('app_organisation', 'this application')
 
-        self.write('%s - %s'  % (self.settings['app_title'], self.settings['app_organisation']))
+        db_connection = db.connection()
+        count = db_connection.get('SELECT COUNT(DISTINCT entity_id) AS entities, COUNT(*) AS properties FROM property;')
+
+        self.add_header('Content-Type', 'text/plain; charset=utf-8')
+        self.write('%s\n'  % self.settings['app_title'])
+        self.write('Organisation: %s\n'  % self.settings['app_organisation'])
+        self.write('Entities:     %s\n'  % count.entities)
+        self.write('Properties:   %s\n'  % count.properties)
 
 
 handlers = [
