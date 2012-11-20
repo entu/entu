ad7117acb364d6faf6e60bf42ce270bbc137e8a2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2012-11-20 11:23:22 +0200

    CREATE TABLE `dag_formula`

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -300,18 +300,48 @@ CREATE TABLE `property` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci;
 
 
+<<<<<<< HEAD
 CREATE TABLE `dag_entity` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `entity_id` int(11) unsigned DEFAULT NULL,
   `related_entity_id` int(11) unsigned DEFAULT NULL,
+=======
+CREATE TABLE `dag_formula` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `property_id` int(11) unsigned DEFAULT NULL,
+  `related_property_id` int(11) unsigned DEFAULT NULL,
+  `entity_id` int(11) unsigned DEFAULT NULL,
+  `relationship_definition_keyname` varchar(25) CHARACTER SET ascii COLLATE ascii_bin NOT NULL DEFAULT '',
+  `reversed_relationship_definition_keyname` varchar(25) CHARACTER SET ascii COLLATE ascii_bin NOT NULL DEFAULT '',
+  `entity_definition_keyname` varchar(25) CHARACTER SET ascii COLLATE ascii_bin DEFAULT NULL,
+  `property_definition_keyname` varchar(50) CHARACTER SET ascii COLLATE ascii_bin DEFAULT NULL,
+>>>>>>> CREATE TABLE `dag_formula`
   `created` datetime DEFAULT NULL,
   `created_by` varchar(100) COLLATE utf8_estonian_ci DEFAULT NULL,
   `deleted` datetime DEFAULT NULL,
   `deleted_by` varchar(100) COLLATE utf8_estonian_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
+<<<<<<< HEAD
   KEY `ed_fk_e` (`entity_id`),
   KEY `ed_fk_re` (`related_entity_id`),
   CONSTRAINT `ed_fk_e` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE,
   CONSTRAINT `ed_fk_re` FOREIGN KEY (`related_entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci;
 
+=======
+  KEY `fd_fk_p` (`property_id`),
+  KEY `fd_fk_rp` (`related_property_id`),
+  KEY `fd_fk_e` (`entity_id`),
+  KEY `fd_fk_rdk` (`relationship_definition_keyname`),
+  KEY `fd_fk_rrdk` (`reversed_relationship_definition_keyname`),
+  KEY `fd_fk_edk` (`entity_definition_keyname`),
+  KEY `fd_fk_pdk` (`property_definition_keyname`),
+  CONSTRAINT `fd_fk_pdk` FOREIGN KEY (`property_definition_keyname`) REFERENCES `property_definition` (`keyname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fd_fk_edk` FOREIGN KEY (`entity_definition_keyname`) REFERENCES `entity_definition` (`keyname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fd_fk_rrdk` FOREIGN KEY (`reversed_relationship_definition_keyname`) REFERENCES `relationship_definition` (`keyname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fd_fk_e` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fd_fk_p` FOREIGN KEY (`property_id`) REFERENCES `property` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fd_fk_rdk` FOREIGN KEY (`relationship_definition_keyname`) REFERENCES `relationship_definition` (`keyname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fd_fk_rp` FOREIGN KEY (`related_property_id`) REFERENCES `property` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci;
+>>>>>>> CREATE TABLE `dag_formula`
