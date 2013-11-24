5b3c9c51da12d80d672061e27f74aaefbdf8cf6d
Argo <argo@argo-mbp.lan>
2013-11-24 02:38:25 +0200

    Redirect wrong domains to www.entu.ee

--- app/main/helper.py
+++ app/main/helper.py
@@ -45,60 +45,62 @@ class myDatabase():
     def get_app_settings(self, host=None):
         if not host:
             host = self.request.host
-        if self.__app_settings:
-            return self.__app_settings.get(host, {})
 
-        logging.debug('Loaded app_settings for %s.' % host)
+        if not self.__app_settings:
+            logging.debug('Loaded app_settings for %s.' % host)
 
-        db = torndb.Connection(
-            host     = self.settings['database-host'],
-            database = self.settings['database-database'],
-            user     = self.settings['database-user'],
-            password = self.settings['database-password'],
-        )
-        sql = """
-            SELECT DISTINCT
-                e.id AS entity,
-                property_definition.dataproperty AS property,
-                IF(
-                    property_definition.datatype='decimal',
-                    property.value_decimal,
+            db = torndb.Connection(
+                host     = self.settings['database-host'],
+                database = self.settings['database-database'],
+                user     = self.settings['database-user'],
+                password = self.settings['database-password'],
+            )
+            sql = """
+                SELECT DISTINCT
+                    e.id AS entity,
+                    property_definition.dataproperty AS property,
                     IF(
-                        property_definition.datatype='integer',
-                        property.value_integer,
+                        property_definition.datatype='decimal',
+                        property.value_decimal,
                         IF(
-                            property_definition.datatype='file',
-                            property.value_file,
-                            property.value_string
+                            property_definition.datatype='integer',
+                            property.value_integer,
+                            IF(
+                                property_definition.datatype='file',
+                                property.value_file,
+                                property.value_string
+                            )
                         )
-                    )
-                ) AS value
-            FROM (
-                SELECT
-                    entity.id,
-                    entity.entity_definition_keyname
-                FROM
-                    entity,
-                    relationship
-                WHERE relationship.related_entity_id = entity.id
-                AND entity.is_deleted = 0
-                AND relationship.is_deleted = 0
-                AND relationship.relationship_definition_keyname = 'child'
-                AND relationship.entity_id IN (%s)
-            ) AS e
-            LEFT JOIN property_definition ON property_definition.entity_definition_keyname = e.entity_definition_keyname AND property_definition.is_deleted = 0
-            LEFT JOIN property ON property.property_definition_keyname = property_definition.keyname AND property.entity_id = e.id AND property.is_deleted = 0;
-        """ % self.settings['customergroup']
-        customers = {}
-        for c in db.query(sql):
-            customers.setdefault(c.entity, {})[c.property] = c.value
-        hosts = {}
-        for c in customers.values():
-            hosts[c.get('domain', '')] = c
+                    ) AS value
+                FROM (
+                    SELECT
+                        entity.id,
+                        entity.entity_definition_keyname
+                    FROM
+                        entity,
+                        relationship
+                    WHERE relationship.related_entity_id = entity.id
+                    AND entity.is_deleted = 0
+                    AND relationship.is_deleted = 0
+                    AND relationship.relationship_definition_keyname = 'child'
+                    AND relationship.entity_id IN (%s)
+                ) AS e
+                LEFT JOIN property_definition ON property_definition.entity_definition_keyname = e.entity_definition_keyname AND property_definition.is_deleted = 0
+                LEFT JOIN property ON property.property_definition_keyname = property_definition.keyname AND property.entity_id = e.id AND property.is_deleted = 0;
+            """ % self.settings['customergroup']
+            customers = {}
+            for c in db.query(sql):
+                customers.setdefault(c.entity, {})[c.property] = c.value
 
-        self.__app_settings = hosts
+            self.__app_settings = {}
+            for c in customers.values():
+                self.__app_settings[c.get('domain', '')] = c
 
-        return hosts.get(self.request.host, {})
+        if not self.__app_settings.get(self.request.host):
+            self.redirect('https://www.entu.ee')
+            return
+
+        return self.__app_settings.get(self.request.host, {})
 
 
 class myUser():
