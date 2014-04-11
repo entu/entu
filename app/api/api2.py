2530b0d09b58adfbe470036d556ca22f0aec45e9
Argo Roots <argo@roots.ee>
2014-04-11 13:25:49 +0300

    API2 file upload method; Comments;

--- app/api/api2.py
+++ app/api/api2.py
@@ -46,6 +46,10 @@ class API2EntityList(myRequestHandler, Entity2):
 class API2Entity(myRequestHandler, Entity):
     @web.removeslash
     def get(self, entity_id):
+        """
+        Get entity (with given ID)
+
+        """
         entity = self.get_entities(entity_id=entity_id, limit=1)
         if not entity:
             return self.json({
@@ -60,7 +64,50 @@ class API2Entity(myRequestHandler, Entity):
 
 
     @web.removeslash
+    def put(self, entity_id):
+        """
+        Change entity (with given ID) properties
+
+        """
+        if not self.current_user:
+            return self.json({
+                'error': 'Forbidden!',
+                'time': round(self.request.request_time(), 3),
+            }, 403)
+
+        if not entity_id:
+            return self.json({
+                'error': 'No entity ID!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        # entity = self.get_entities(entity_id=entity_id, limit=1)
+        # if not entity:
+        #     return self.json({
+        #         'error': 'Entity with given ID is not found!',
+        #         'time': round(self.request.request_time(), 3),
+        #     }, 404)
+
+        for dataproperty, value in self.request.arguments.iteritems():
+            if dataproperty in ['user', 'policy', 'signature']:
+                continue
+            if type(value) is not list:
+                value = [value]
+            for v in value:
+                new_property_id = self.set_property(entity_id=entity_id, property_definition_keyname=dataproperty, value=v)
+
+        self.json({
+            'entity_id': entity_id,
+            'time': round(self.request.request_time(), 3),
+        }, 201)
+
+
+    @web.removeslash
     def post(self, entity_id):
+        """
+        Create new child entity (under entity with given ID)
+
+        """
         if not self.current_user:
             return self.json({
                 'error': 'Forbidden!',
@@ -96,40 +143,11 @@ class API2Entity(myRequestHandler, Entity):
 
 
     @web.removeslash
-    def put(self, entity_id):
-        if not self.current_user:
-            return self.json({
-                'error': 'Forbidden!',
-                'time': round(self.request.request_time(), 3),
-            }, 403)
-
-        if not entity_id:
-            return self.json({
-                'error': 'No entity ID!',
-                'time': round(self.request.request_time(), 3),
-            }, 400)
-
-        # entity = self.get_entities(entity_id=entity_id, limit=1)
-        # if not entity:
-        #     return self.json({
-        #         'error': 'Entity with given ID is not found!',
-        #         'time': round(self.request.request_time(), 3),
-        #     }, 404)
-
-        properties = self.get_arguments('properties', strip=True)
-        logging.debug(self.request)
-        # if not definition:
-        #     return self.json({
-        #         'error': 'No definition!',
-        #         'time': round(self.request.request_time(), 3),
-        #     }, 400)
-
-        # entity_id = self.create_entity(entity_definition_keyname=definition, parent_entity_id=entity_id)
-
-        self.json({
-            # 'entity_id': entity_id,
-            'time': round(self.request.request_time(), 3),
-        }, 201)
+    def delete(self, entity_id):
+        """
+        Delete entity (with given ID)
+        """
+        pass
 
 
 class API2EntityChilds(myRequestHandler, Entity2):
@@ -182,6 +200,71 @@ class API2EntityReferrals(myRequestHandler, Entity2):
             'time': round(self.request.request_time(), 3),
             'count': db_result.get('count', 0),
         })
+
+
+class API2FileUpload(myRequestHandler, Entity):
+    @web.removeslash
+    def post(self):
+        """
+        Upload/create file
+        """
+        if not self.current_user:
+            return self.json({
+                'error': 'Forbidden!',
+                'time': round(self.request.request_time(), 3),
+            }, 403)
+
+        uploaded_file = self.request.body
+        if not uploaded_file:
+            return self.json({
+                'error': 'No file!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        try:
+            file_size = int(self.request.headers.get('Content-Length', 0))
+        except Exception:
+            return self.json({
+                'error': 'Content-Length header not set!',
+                'time': round(self.request.request_time(), 3),
+            }, 403)
+
+        if file_size != len(uploaded_file):
+            logging.debug(len(uploaded_file))
+            return self.json({
+                'error': 'File not complete!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        entity_id = self.get_argument('entity', default=None, strip=True)
+        if not entity_id:
+            return self.json({
+                'error': 'No entity ID!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        # entity = self.get_entities(entity_id=entity_id, limit=1)
+        # if not entity:
+        #     return self.json({
+        #         'error': 'Entity with given ID is not found!',
+        #         'time': round(self.request.request_time(), 3),
+        #     }, 404)
+
+        dataproperty = self.get_argument('property', default=None, strip=True)
+        if not dataproperty:
+            return self.json({
+                'error': 'No property!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        filename = self.get_argument('filename', default=None, strip=True)
+        if not filename:
+            return self.json({
+                'error': 'No filename!',
+                'time': round(self.request.request_time(), 3),
+            }, 400)
+
+        self.new_property_id = self.set_property(entity_id=entity_id, property_definition_keyname=dataproperty, value={'filename': filename, 'body': uploaded_file, 'is_link': 0})
 
 
 class API2EntityPicture(myRequestHandler, Entity2):
@@ -287,6 +370,8 @@ handlers = [
     (r'/api2/entity-(.*)/referrals', API2EntityReferrals),
     (r'/api2/entity-(.*)/picture', API2EntityPicture),
     (r'/api2/entity-(.*)', API2Entity),
+    (r'/api2/file', API2FileUpload),
+    # (r'/api2/file-(.*)', API2File),
     (r'/api2/definition', API2Definition),
     (r'/api2/s3upload', S3FileUpload),
     (r'/api2(.*)', API2NotFound),
