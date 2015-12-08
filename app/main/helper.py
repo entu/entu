3784f5b8299a8895dee9e7d8546262c3bdef9ac9
Argo Roots <argo@roots.ee>
2015-12-08 12:27:31 +0200

    Bugfix

--- app/main/helper.py
+++ app/main/helper.py
@@ -165,7 +165,7 @@ class myUser(myE):
             return self.__user
 
         try:
-            session = list(rethinkdb.table('session').get_all(session_key, {'index': 'key'}).limit(1).run(self.rethinkdb('entu')))[0]
+            session = list(rethinkdb.table('session').get_all(session_key, index='key').limit(1).run(self.rethinkdb('entu')))[0]
         except Exception, e:
             self.captureException()
             logging.error(e)
