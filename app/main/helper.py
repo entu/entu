10158c959302e3b2509cc7e743c2a59606a6a4ba
Argo Roots <argo@roots.ee>
2017-08-17 12:13:13 +0300

    Save mongo connection and not connection to database

--- app/main/helper.py
+++ app/main/helper.py
@@ -302,9 +302,9 @@ class myDatabase():
             x = self.settings['mongodbs'][database].server_info()
         except Exception, e:
             logging.error(e)
-            self.settings['mongodbs'][database] = MongoClient(self.settings['mongodb'], ssl=True, connect=True)[database]
+            self.settings['mongodbs'][database] = MongoClient(self.settings['mongodb'], ssl=True, connect=True)
             logging.error(database + ' mongo')
-        return self.settings['mongodbs'][database]
+        return self.settings['mongodbs'][database][database]
 
 
 
