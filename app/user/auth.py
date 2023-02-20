f302f816424db38882bba11c26c7b92efb5c0893
Argo Roots <argo@roots.ee>
2023-02-20 20:38:37 +0200

    SSLContext fix

--- app/user/auth.py
+++ app/user/auth.py
@@ -97,7 +97,7 @@ class AuthOAuth2(myRequestHandler, auth.OAuth2Mixin):
                 'code':             self.get_argument('code', None),
                 'grant_type':       'authorization_code',
             }),
-            ssl_options = {'validate_cert': False},
+            ssl_options = ssl.SSLContext(validate_cert=False),
             callback = self._got_token,
         )
 
@@ -118,7 +118,7 @@ class AuthOAuth2(myRequestHandler, auth.OAuth2Mixin):
 
         httpclient.AsyncHTTPClient().fetch(self.oauth2_provider['info_url'],
             headers = {'Authorization': 'Bearer %s' % access_token},
-            ssl_options = {'validate_cert': False},
+            ssl_options = ssl.SSLContext(validate_cert=False),
             callback = self._got_user
         )
 
